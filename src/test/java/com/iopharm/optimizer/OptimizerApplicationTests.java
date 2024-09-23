a711b7841474350ad7dfe9cf64695e1280ed9cb1
mohamed <Mohamedashraf814@yahoo.com>
2024-09-24 00:43:31 +0300

    initial commit

---
+++ src/test/java/com/iopharm/optimizer/OptimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iopharm.optimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
